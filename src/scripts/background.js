5b8d58f8425679f3d3bf6ef989907e6cd28131ec
Martial Séron <martial.seron@alturing.eu>
2021-10-13 21:12:20 +0000

    remove license management

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,50 +1,3 @@
-/* global menu:false */
-function getLicense(cb) {
-  console.group('getLicense()'); // eslint-disable-line
-  chrome.identity.getAuthToken({ interactive: true }, (token) => {
-    console.log('token :', token); // eslint-disable-line
-
-    const CWS_LICENSE_API_URL = 'https://www.googleapis.com/chromewebstore/v1.1/userlicenses/';
-    const req = new XMLHttpRequest();
-    console.log('chrome.runtime.id :', chrome.runtime.id); // eslint-disable-line
-    const appId = chrome.runtime.id;
-    req.open('GET', CWS_LICENSE_API_URL + appId);
-    req.setRequestHeader('Authorization', `Bearer ${token}`);
-    req.onreadystatechange = () => {
-      if (req.readyState === 4) {
-        const license = JSON.parse(req.responseText);
-        console.log('license :', license); // eslint-disable-line
-
-        if (!license.error) {
-          chrome.storage.sync.set({ license }, () => {
-            console.log('license synced'); // eslint-disable-line
-            console.groupEnd(); // eslint-disable-line
-            return cb && cb(license);
-          });
-        }
-      }
-    };
-    req.send();
-  });
-}
-
-function checkLicense(cb) {
-  console.group('checkLicense()'); // eslint-disable-line
-  chrome.storage.sync.get('license', (data) => {
-    // console.info('data :', data);
-    // console.info('data.license :', data.license);
-    // console.info('Object.keys(data.license) :', Object.keys(data.license));
-
-    if (!data.license || !Object.keys(data.license).length || data.license.error) {
-      // console.log('no license');
-      return getLicense(cb);
-    }
-    // console.log(data.license);
-    console.groupEnd(); // eslint-disable-line
-    return cb && cb(data.license);
-  });
-}
-
 /**
  * Recieves messages from content
  */
@@ -55,35 +8,12 @@ chrome.runtime.onMessage.addListener((message) => {
   }
 });
 
-
 chrome.commands.onCommand.addListener((e) => {
-  // Test if user purchased extension
-  checkLicense((license) => {
-    chrome.management.getSelf((extInfo) => {
-      console.log('extInfo : ', extInfo); // eslint-disable-line
-      chrome.identity.getProfileUserInfo((userInfo) => {
-        const userOK = userInfo && userInfo.Id === '107371038368034484502';
-        const licenseOK = license && license.accessLevel === 'FULL';
-        const devMode = extInfo && extInfo.installType === 'development';
-
-          if ( userOK || licenseOK || devMode) {
-            chrome.tabs.getSelected((currentTab) => {
-              chrome.tabs.sendMessage(currentTab.id, {
-                from: 'context_menu',
-                method: 'convert_text',
-                params: { action: e },
-              });
-            });
-          } else {
-            alert(chrome.i18n.getMessage('alert_feature_forbidden'));
-          }
-
-        console.log('userInfo : ', userInfo); // eslint-disable-line
-      });
+  chrome.tabs.getSelected((currentTab) => {
+    chrome.tabs.sendMessage(currentTab.id, {
+      from: 'context_menu',
+      method: 'convert_text',
+      params: { action: e },
     });
   });
 });
-
-// chrome.storage.sync.clear(() => {
-checkLicense();
-// })
